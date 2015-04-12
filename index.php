ef797b9e208509f0e9849bf9a906e991734f53b7
Philip Rudy <me@philiparudy.com>
2015-04-12 18:29:23 -0400

    First Commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+
+</head>
+<body>
+
+
+	 <header>
+		 <nav id="main-nav">
+			 <ul>
+				 <li><a href="">Home</a></li>
+				 <li><a href="videos">Videos</a>
+				 <li><a href="blog">Blog</a></li>
+				 <li><a href="About">Contact</a></li>
+				 <li><a href="Submit">Submit</a></li>
+				 </li>
+			 </ul>
+		 </nav>
+				 
+	 
+	 </header>	
+	 
+	 
+	 
+	 
+	 
+	 <section id="main-body">
+	 
+	 </section>
+  
+  
+  
+  
+	  
+	 <footer>
+	 </footer>  
+	 
+	 
+	 
+	 
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</body>
+</html>
